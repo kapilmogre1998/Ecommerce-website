d84c3ba996aa6fa31dc4ea2c7fdb8216e83905e6
kapilmogre1998 <kapil.mogre@nobroker.in>
2023-04-27 23:43:52 +0530

    changed the position from sticky to fixed

--- ecommerce-app/src/components/Home/Header.jsx
+++ ecommerce-app/src/components/Home/Header.jsx
@@ -2,9 +2,8 @@ import './Header.scss'
 
 const Header = (props) => {
     const {headerSticky} = props;
-    console.log({headerSticky});
     return (
-        <header className={`flex items-center justify-between header-container ${headerSticky ? 'sticky shadow-md' : ''}`} >
+        <header className={`flex items-center justify-between header-container ${headerSticky ? 'fixed shadow-md' : ''}`} >
             <div>
                 <img className='rounded-md' width={80} height={80} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_fUC8vkLHUkBie0556QT_LhoNJNrMKdIn55U-KK1xOw&usqp=CAU&ec=48665701" alt="" />
             </div>
