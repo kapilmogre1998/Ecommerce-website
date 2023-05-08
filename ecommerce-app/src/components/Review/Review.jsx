ad84fc8c6da4515097f1ff13e745c0a226578659
kapilmogre1998 <kapil.mogre@nobroker.in>
2023-05-09 00:27:03 +0530

    media query in review section

--- ecommerce-app/src/components/Review/Review.jsx
+++ ecommerce-app/src/components/Review/Review.jsx
@@ -2,17 +2,19 @@ import './Review.scss';
 
 const Review = () => {
     return (
-        <div className='review-container' >
+        <div className='customer-container' >
             <div className='font-medium text-xl mt-4 mb-6 text-center' >Customer Review</div>
-            <div className='reviews grid gap-4' >
+            <div className='customer-reviews flex gap-6' >
                 {
-                    new Array(3).fill(0).map(() => (
-                        <div className='customer-review flex flex-col items-center text-center py-6 px-5 justify-around' >
+                    new Array(3).fill(0).map((_,i) => (
+                        <div key={i} className='flex flex-col review-container' >
                             <div className='customer-img' >
-                                <img className='rounded-full w-full h-full' loading='lazy' src="src/assets/review2.jpeg" alt="img" />
+                                <img className='rounded w-full h-full object-cover' loading='lazy' src="src/assets/team-1.jpg" alt="img" />
                             </div>
-                            <div className='text-sm mt-4 font-medium' >&#x201C;Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit corrupti sit inventore nam soluta ab, odio totam ipsam quaerat? Quam architecto eveniet excepturi exercitationem iste dolorem nisi et accusamus molestiae.&#x201C;</div>
-                        </div>
+                            <div className='review-text flex flex-col items-center text-left justify-around' >
+                                <div className='text-sm mt-4 font-medium' >&#x201C;Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit corrupti sit inventore nam soluta ab, odio totam ipsam quaerat? Quam architecto eveniet excepturi exercitationem iste dolorem nisi et accusamus molestiae.&#x201C;</div>
+                            </div>
+                        </div> 
                     ))
                 }
             </div>
